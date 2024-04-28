73d5feea2cb13b1363215712932f13f081ee7b92
AranzaMxz <amunozc2002@alumno.ipn.mx>
2024-04-27 22:54:30 -0600

    Functions added

--- src/game/rules.py
+++ src/game/rules.py
@@ -1,7 +1,13 @@
 
 # Verificate the enter number is in the range 
-def verificateSelection(selection, boardSize):
-    if selection > boardSize**2:
+def inputAccepted(selection, boardSize):
+    if selection > boardSize**2 or selection < 1:
         print("Your selection is out of range. Try again")
     else:
         return True
+
+def isFree(row, col, values):
+    if values[row][col] == '  ':
+        return True
+    else:
+        return False
