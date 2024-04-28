8a2d6643480ef791aa46b094961a95a9719ae04b
AranzaMxz <amunozc2002@alumno.ipn.mx>
2024-04-28 14:12:54 -0600

    Update scores.py

--- src/game/scores.py
+++ src/game/scores.py
@@ -2,8 +2,8 @@
 def printScores(player1, player2):
     print("\t-----------------------")
     print("\t         SCORE")
-    print("\t      {}         {}".format(player1.name, player2.name))
-    print("\t      {}         {}".format(player1.playswon, player2.playswon))
+    print("\t   {}         {}".format(player1.name, player2.name))
+    print("\t   {}            {}".format(player1.playswon, player2.playswon))
     print("\t-----------------------")
 
 def scores(player1, player2):
