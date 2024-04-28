9eb6015a0896ddf4f22928d3b1b94a18c9ff0f74
AranzaMxz <amunozc2002@alumno.ipn.mx>
2024-04-27 22:53:57 -0600

    Update winData.py
    Values changed in the winning list

--- src/data/winData.py
+++ src/data/winData.py
@@ -3,14 +3,14 @@ def winPlays(n):
     winList = []
     # Rows and columns
     for i in range(n): # values from 0 to n-1
-        row = [(i, j) for j in range(n)]
-        column = [(j, i) for j in range(n)]
+        row = [n * i + j + 1  for j in range(n)]
+        column = [n * j + i + 1 for j in range(n)]
         winList.append(row)
         winList.append(column)
 
     # Diagonals
-    diagonal1 = [(i, i) for i in range(n)]
-    diagonal2 = [(i, n - i - 1) for i in range(n)]
+    diagonal1 = [n * i +  i + 1 for i in range(n)]
+    diagonal2 = [n * i + (n - i) for i in range(n)]
     winList.append(diagonal1)
     winList.append(diagonal2)
 
